68f3a863891fcb3a85fbab886154650c8c9f8f68
Mike Skells <mike.skells@talk21.com>
2017-01-31 00:20:51 +0000

    first cut parallelisation

--- src/compiler/scala/tools/nsc/backend/jvm/GenBCode.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/GenBCode.scala
@@ -9,9 +9,13 @@ package tools.nsc
 package backend
 package jvm
 
-import scala.collection.mutable
-import scala.reflect.internal.util.Statistics
+import java.util.concurrent.BlockingQueue
 
+import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.{Future, Promise}
+import scala.reflect.internal.util.Statistics
 import scala.tools.asm
 import scala.tools.asm.tree.ClassNode
 import scala.tools.nsc.backend.jvm.opt.ByteCodeRepository
@@ -67,13 +71,15 @@ abstract class GenBCode extends BCodeSyncAndTry {
     private var mirrorCodeGen   : JMirrorBuilder   = null
     private var beanInfoCodeGen : JBeanInfoBuilder = null
 
+    class Workflow {
+      val item2 = Promise[Item2]
+      val item3 = Promise[Item3]
+    }
     /* ---------------- q1 ---------------- */
 
-    case class Item1(arrivalPos: Int, cd: ClassDef, cunit: CompilationUnit) {
-      def isPoison = { arrivalPos == Int.MaxValue }
-    }
-    private val poison1 = Item1(Int.MaxValue, null, null)
-    private val q1 = new java.util.LinkedList[Item1]
+    case class Item1(arrivalPos: Int, cd: ClassDef, cunit: CompilationUnit, workflow :Workflow)
+
+    private val q1 = new ArrayBuffer[Item1]
 
     /* ---------------- q2 ---------------- */
 
@@ -82,12 +88,8 @@ abstract class GenBCode extends BCodeSyncAndTry {
                      plain:          asm.tree.ClassNode,
                      bean:           asm.tree.ClassNode,
                      sourceFilePath: String,
-                     outFolder:      scala.tools.nsc.io.AbstractFile) {
-      def isPoison = { arrivalPos == Int.MaxValue }
-    }
-
-    private val poison2 = Item2(Int.MaxValue, null, null, null, null, null)
-    private val q2 = new _root_.java.util.LinkedList[Item2]
+                     outFolder:      scala.tools.nsc.io.AbstractFile)
+    private val q2 = new java.util.concurrent.LinkedBlockingQueue[Workflow]
 
     /* ---------------- q3 ---------------- */
 
@@ -107,66 +109,95 @@ abstract class GenBCode extends BCodeSyncAndTry {
                      mirror:     SubItem3,
                      plain:      SubItem3,
                      bean:       SubItem3,
-                     outFolder:  scala.tools.nsc.io.AbstractFile) {
-
-      def isPoison  = { arrivalPos == Int.MaxValue }
+                     outFolder:  scala.tools.nsc.io.AbstractFile){
     }
-    private val i3comparator = new java.util.Comparator[Item3] {
-      override def compare(a: Item3, b: Item3) = {
-        if (a.arrivalPos < b.arrivalPos) -1
-        else if (a.arrivalPos == b.arrivalPos) 0
-        else 1
-      }
-    }
-    private val poison3 = Item3(Int.MaxValue, null, null, null, null)
-    private val q3 = new java.util.PriorityQueue[Item3](1000, i3comparator)
+    private val q3 = new java.util.concurrent.LinkedBlockingQueue[Workflow]
 
     /*
      *  Pipeline that takes ClassDefs from queue-1, lowers them into an intermediate form, placing them on queue-2
+     *  This runs in the main thread as it requires access to the tree
      */
-    class Worker1(needsOutFolder: Boolean) {
+     private class Worker1(needsOutFolder: Boolean) {
+      //TODO should be a scalac param
+      val checkCaseInsensitively = true
+      val optAddToBytecodeRepository = settings.optAddToBytecodeRepository
+      val optBuildCallGraph = settings.optBuildCallGraph
 
       val caseInsensitively = mutable.Map.empty[String, Symbol]
 
-      def run() {
-        while (true) {
-          val item = q1.poll
-          if (item.isPoison) {
-            q2 add poison2
-            return
+      import scala.util.{Try, Success, Failure}
+
+      def run() = {
+        val globalOptimisations = hasGlobalOptimisations
+        val pendingStarts = new ArrayBuffer[(Item1, Try[Item2])](q1.size)
+        q1 foreach { item1 =>
+          val item2Try = Try {
+            withCurrentUnitNoLog(item1.cunit)(visit(item1))
           }
-          else {
-            try   { withCurrentUnitNoLog(item.cunit)(visit(item)) }
-            catch {
-              case ex: Throwable =>
-                ex.printStackTrace()
-                error(s"Error while emitting ${item.cunit.source}\n${ex.getMessage}")
-            }
+          item2Try match {
+            case Success(item2) =>
+              runOptimizations(item2)
+            case Failure(ex: Throwable) =>
+              ex.printStackTrace()
+              reporter.error(NoPosition, s"Error while emitting ${item1.cunit.source}\n${ex.getMessage}")
           }
+          if (globalOptimisations) pendingStarts += ((item1, item2Try))
+          else startPipeline(item1,item2Try)
+        }
+        runGlobalOptimizations()
+        if (globalOptimisations)
+          pendingStarts foreach {
+            case (item1, item2try) => startPipeline (item1,item2try)
+          }
+      }
+      def startPipeline(item1:Item1, item2:Try[Item2]) = {
+        item1.workflow.item2.complete(item2)
+      }
+
+      def runOptimizations(item:Item2): Unit = {
+        // add classes to the bytecode repo before building the call graph: the latter needs to
+        // look up classes and methods in the code repo.
+        if (optAddToBytecodeRepository) {
+            val someSourceFilePath = Some(item.sourceFilePath)
+            if (item.mirror != null) byteCodeRepository.add(item.mirror, someSourceFilePath)
+            if (item.plain != null) byteCodeRepository.add(item.plain, someSourceFilePath)
+            if (item.bean != null) byteCodeRepository.add(item.bean, someSourceFilePath)
+        }
+        if (optBuildCallGraph) {
+          // skip call graph for mirror / bean: wd don't inline into them, and they are not used in the plain class
+          if (item.plain != null) callGraph.addClass(item.plain)
         }
       }
+      def hasGlobalOptimisations = settings.optInlinerEnabled || settings.optClosureInvocations
+      def runGlobalOptimizations(): Unit = {
+        if (settings.optInlinerEnabled)
+          bTypes.inliner.runInliner()
+        if (settings.optClosureInvocations)
+          closureOptimizer.rewriteClosureApplyInvocations()
+      }
+
 
       /*
        *  Checks for duplicate internal names case-insensitively,
        *  builds ASM ClassNodes for mirror, plain, and bean classes;
-       *  enqueues them in queue-2.
+       *  returns an Item2
        *
        */
-      def visit(item: Item1) {
-        val Item1(arrivalPos, cd, cunit) = item
+      def visit(item: Item1) = {
+        val Item1(arrivalPos, cd, cunit, _) = item
         val claszSymbol = cd.symbol
 
-        // GenASM checks this before classfiles are emitted, https://github.com/scala/scala/commit/e4d1d930693ac75d8eb64c2c3c69f2fc22bec739
-        val lowercaseJavaClassName = claszSymbol.javaClassName.toLowerCase
-        caseInsensitively.get(lowercaseJavaClassName) match {
-          case None =>
-            caseInsensitively.put(lowercaseJavaClassName, claszSymbol)
-          case Some(dupClassSym) =>
-            reporter.warning(
-              claszSymbol.pos,
-              s"Class ${claszSymbol.javaClassName} differs only in case from ${dupClassSym.javaClassName}. " +
-              "Such classes will overwrite one another on case-insensitive filesystems."
-            )
+        if (checkCaseInsensitively) {
+          // GenBCode checks this before classfiles are emitted, https://github.com/scala/scala/commit/e4d1d930693ac75d8eb64c2c3c69f2fc22bec739
+          val lowercaseJavaClassName = claszSymbol.javaClassName.toLowerCase
+          caseInsensitively.put(lowercaseJavaClassName, claszSymbol).foreach {
+            dupClassSym =>
+              reporter.warning(
+                claszSymbol.pos,
+                s"Class ${claszSymbol.javaClassName} differs only in case from ${dupClassSym.javaClassName}. " +
+                  "Such classes will overwrite one another on case-insensitive filesystems."
+              )
+          }
         }
 
         // shim for SBT, see https://github.com/sbt/sbt/issues/2076
@@ -201,48 +232,86 @@ abstract class GenBCode extends BCodeSyncAndTry {
             )
           } else null
 
-          // ----------- hand over to pipeline-2
+        // ----------- hand over to pipeline-2
 
-        val item2 =
-          Item2(arrivalPos,
-                mirrorC, plainC, beanC,
-                cunit.source.file.canonicalPath,
-                outF)
-
-        q2 add item2 // at the very end of this method so that no Worker2 thread starts mutating before we're done.
+        Item2(arrivalPos,
+          mirrorC, plainC, beanC,
+          cunit.source.file.canonicalPath,
+          outF)
 
       } // end of method visit(Item1)
 
     } // end of class BCodePhase.Worker1
 
+    abstract class ParallelWorker[I <: AnyRef,O](val id: Int, val queue: BlockingQueue[Workflow], val timer:Statistics.Timer) extends Runnable {
+
+      import scala.concurrent.{Await, Future}
+      import scala.concurrent.duration.Duration
+      import scala.util.{Success, Failure}
+
+      def getWork(workflow: Workflow): Future[I]
+
+      def process(input: I): O
+
+      def nextStageSuccess(workflow: Workflow, result: O): Unit
+
+      def nextStageFailed(workflow: Workflow, ex: Throwable): Unit
+
+      @tailrec final def run() : Unit = {
+        val currentWork = queue.poll()
+        if (currentWork ne null) {
+          val work = getWork(currentWork)
+          Await.ready(work, Duration.Inf)
+          work.value.get match {
+            case Success(item) =>
+              val start = timer.start()
+              try {
+                process(item)
+                nextStageSuccess(currentWork, process(item))
+              } catch {
+                case t: Throwable =>
+                  nextStageFailed(currentWork, t)
+              }
+              timer.stop(start)
+            case Failure(f) => //TODO
+              nextStageFailed(currentWork, f)
+          }
+
+          run()
+        }
+      }
+    }
+
+    val ec = scala.concurrent.ExecutionContext.fromExecutorService(
+      java.util.concurrent.Executors.newCachedThreadPool(), onWorkerError
+    )
+    def onWorkerError(workerFailed:Throwable) : Unit = {
+      workerFailed.printStackTrace()
+      reporter.error(NoPosition, workerFailed.toString)
+    }
+    object Worker2 {
+      import scala.concurrent.Future
+      def startWorkers()= for (i <- 1 to 3) yield {
+        val worker = new Worker2(i)
+        Future(worker)(ec)
+      }
+    }
     /*
      *  Pipeline that takes ClassNodes from queue-2. The unit of work depends on the optimization level:
      *
      *    (a) no optimization involves:
      *          - converting the plain ClassNode to byte array and placing it on queue-3
      */
-    class Worker2 {
-      def runGlobalOptimizations(): Unit = {
-        import scala.collection.JavaConverters._
+    class Worker2(id:Int) extends ParallelWorker[Item2, Item3](id, q2, BackendStats.bytesGenStat) {
 
-        // add classes to the bytecode repo before building the call graph: the latter needs to
-        // look up classes and methods in the code repo.
-        if (settings.optAddToBytecodeRepository) q2.asScala foreach {
-          case Item2(_, mirror, plain, bean, sourceFilePath, _) =>
-            val someSourceFilePath = Some(sourceFilePath)
-            if (mirror != null) byteCodeRepository.add(mirror, someSourceFilePath)
-            if (plain != null)  byteCodeRepository.add(plain, someSourceFilePath)
-            if (bean != null)   byteCodeRepository.add(bean, someSourceFilePath)
-        }
-        if (settings.optBuildCallGraph) q2.asScala foreach { item =>
-          // skip call graph for mirror / bean: wd don't inline into tem, and they are not used in the plain class
-          if (item.plain != null) callGraph.addClass(item.plain)
-        }
-        if (settings.optInlinerEnabled)
-          bTypes.inliner.runInliner()
-        if (settings.optClosureInvocations)
-          closureOptimizer.rewriteClosureApplyInvocations()
-      }
+      override def getWork(workflow: Workflow): Future[Item2] = workflow.item2.future
+
+      override def nextStageSuccess(workflow: Workflow, result: Item3): Unit =
+        workflow.item3.success(result)
+
+      override def nextStageFailed(workflow: Workflow, ex: Throwable): Unit =
+        workflow.item3.failure(ex)
+
 
       def localOptimizations(classNode: ClassNode): Unit = {
         BackendStats.timed(BackendStats.methodOptTimer)(localOpt.methodOptimizations(classNode))
@@ -253,38 +322,29 @@ abstract class GenBCode extends BCodeSyncAndTry {
         addInnerClasses(classNode, bTypes.backendUtils.collectNestedClasses(classNode))
       }
 
-      def run() {
-        runGlobalOptimizations()
-
-        while (true) {
-          val item = q2.poll
-          if (item.isPoison) {
-            q3 add poison3
-            return
-          }
-          else {
-            try {
-              localOptimizations(item.plain)
-              setInnerClasses(item.plain)
-              val lambdaImplMethods = getIndyLambdaImplMethods(item.plain.name)
-              if (lambdaImplMethods.nonEmpty)
-                backendUtils.addLambdaDeserialize(item.plain, lambdaImplMethods)
-              setInnerClasses(item.mirror)
-              setInnerClasses(item.bean)
-              addToQ3(item)
-          } catch {
-              case e: java.lang.RuntimeException if e.getMessage != null && (e.getMessage contains "too large!") =>
-                reporter.error(NoPosition,
-                  s"Could not write class ${item.plain.name} because it exceeds JVM code size limits. ${e.getMessage}")
-              case ex: Throwable =>
-                ex.printStackTrace()
-                error(s"Error while emitting ${item.plain.name}\n${ex.getMessage}")
-            }
-          }
+      override def process(item: Item2): Item3 = {
+        try {
+          localOptimizations(item.plain)
+          setInnerClasses(item.plain)
+          val lambdaImplMethods = getIndyLambdaImplMethods(item.plain.name)
+          if (lambdaImplMethods.nonEmpty)
+            backendUtils.addLambdaDeserialize(item.plain, lambdaImplMethods)
+          setInnerClasses(item.mirror)
+          setInnerClasses(item.bean)
+          makeItem3(item)
+        } catch {
+          case e: java.lang.RuntimeException if e.getMessage != null && (e.getMessage contains "too large!") =>
+            reporter.error(NoPosition,
+              s"Could not write class ${item.plain.name} because it exceeds JVM code size limits. ${e.getMessage}")
+            throw e
+          case ex: Throwable =>
+            ex.printStackTrace()
+            reporter.error(NoPosition, s"Error while emitting ${item.plain.name}\n${ex.getMessage}")
+            throw ex
         }
       }
 
-      private def addToQ3(item: Item2) {
+      private def makeItem3(item: Item2) = {
 
         def getByteArray(cn: asm.tree.ClassNode): Array[Byte] = {
           val cw = new CClassWriter(extraProc)
@@ -304,7 +364,7 @@ abstract class GenBCode extends BCodeSyncAndTry {
           if (beanC != null) AsmUtils.traceClass(beanC.jclassBytes)
         }
 
-        q3 add Item3(arrivalPos, mirrorC, plainC, beanC, outFolder)
+        Item3(arrivalPos, mirrorC, plainC, beanC, outFolder)
 
       }
 
@@ -375,28 +435,46 @@ abstract class GenBCode extends BCodeSyncAndTry {
      *    (d) serialize to disk by draining queue-3.
      */
     private def buildAndSendToDisk(needsOutFolder: Boolean) {
+      val workers2 = Worker2.startWorkers()
+      val workers3 = Worker3.startWorkers()
 
       feedPipeline1()
       val genStart = Statistics.startTimer(BackendStats.bcodeGenStat)
       (new Worker1(needsOutFolder)).run()
       Statistics.stopTimer(BackendStats.bcodeGenStat, genStart)
 
-      (new Worker2).run()
+      (workers2 ++ workers3) foreach {
+        // check for any exception during the operation of the backgroud threads
+        f => scala.concurrent.Await.result(f, scala.concurrent.duration.Duration.Inf)
+      }
+      assert(ec.shutdownNow().isEmpty)
 
-      val writeStart = Statistics.startTimer(BackendStats.bcodeWriteTimer)
-      drainQ3()
-      Statistics.stopTimer(BackendStats.bcodeWriteTimer, writeStart)
+      // we're done
+      assert(q1.isEmpty, s"Some ClassDefs remained in the first queue: $q1")
+      assert(q2.isEmpty, s"Some classfiles remained in the second queue: $q2")
+      assert(q3.isEmpty, s"Some classfiles weren't written to disk: $q3")
 
     }
 
     /* Feed pipeline-1: place all ClassDefs on q1, recording their arrival position. */
     private def feedPipeline1() {
       super.run()
-      q1 add poison1
     }
 
+    object Worker3 {
+      import scala.concurrent.Future
+      def startWorkers()= for (i <- 1 to 3) yield {
+        val worker = new Worker3(i)
+        Future(worker)(ec)
+      }
+    }
     /* Pipeline that writes classfile representations to disk. */
-    private def drainQ3() {
+    class Worker3(id:Int) extends ParallelWorker[Item3, Unit](id, q3,BackendStats.bcodeWriteTimer) {
+      override def getWork(workflow: Workflow): Future[Item3] = workflow.item3.future
+
+      override def nextStageSuccess(workflow: Workflow, result: Unit): Unit = ()
+
+      override def nextStageFailed(workflow: Workflow, ex: Throwable): Unit = ()
 
       def sendToDisk(cfr: SubItem3, outFolder: scala.tools.nsc.io.AbstractFile) {
         if (cfr != null){
@@ -409,32 +487,18 @@ abstract class GenBCode extends BCodeSyncAndTry {
           }
           catch {
             case e: FileConflictException =>
-              error(s"error writing $jclassName: ${e.getMessage}")
+              reporter.error(NoPosition, s"error writing $jclassName: ${e.getMessage}")
           }
         }
       }
 
-      var moreComing = true
-      // `expected` denotes the arrivalPos whose Item3 should be serialized next
-      var expected = 0
 
-      while (moreComing) {
-        val incoming = q3.poll
-        moreComing   = !incoming.isPoison
-        if (moreComing) {
-          val item = incoming
-          val outFolder = item.outFolder
-          sendToDisk(item.mirror, outFolder)
-          sendToDisk(item.plain,  outFolder)
-          sendToDisk(item.bean,   outFolder)
-          expected += 1
-        }
+      override def process(item: Item3): Unit = {
+        val outFolder = item.outFolder
+        sendToDisk(item.mirror, outFolder)
+        sendToDisk(item.plain,  outFolder)
+        sendToDisk(item.bean,   outFolder)
       }
-
-      // we're done
-      assert(q1.isEmpty, s"Some ClassDefs remained in the first queue: $q1")
-      assert(q2.isEmpty, s"Some classfiles remained in the second queue: $q2")
-      assert(q3.isEmpty, s"Some classfiles weren't written to disk: $q3")
 
     }
 
@@ -445,7 +509,10 @@ abstract class GenBCode extends BCodeSyncAndTry {
           case EmptyTree            => ()
           case PackageDef(_, stats) => stats foreach gen
           case cd: ClassDef         =>
-            q1 add Item1(arrivalPos, cd, cunit)
+            val workflow = new Workflow
+            val item1 = Item1(arrivalPos, cd, cunit, workflow)
+            q1 += item1
+
             arrivalPos += 1
         }
       }
