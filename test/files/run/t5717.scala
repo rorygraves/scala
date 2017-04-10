3a0023a4fb0a7ecd93dd2fd944732d937ffcd96c
Mike Skells <mike.skells@talk21.com>
2017-04-10 20:42:12 +0100

    add test case to ensure files are truncated on generation, and more test when unable to write a file or directory

--- test/files/run/t5717.scala
+++ test/files/run/t5717.scala
@@ -13,9 +13,19 @@ object Test extends StoreReporterDirectTest {
   // Let the compiler tell us output dir
   // val sc = newCompiler("-cp", classpath, "-d", testOutput.path)
   // val out = sc.settings.outputDirs.getSingleOutput.get
+  // TODO check the reported errors
   def show(): Unit = {
     // Don't crash when we find a file 'a' where package 'a' should go.
     scala.reflect.io.File(testOutput.path + "/a").writeAll("a")
     compileCode("package a { class B }")
+    //and the same in th case of a subdirectory
+    compileCode("package a.b { class B }")
+
+    // Don't crash when we find a directory 'a' where class 'a' should go.
+    // or maybe more realistically we dont crash if for some reason we cant write the file
+    val blocking = scala.reflect.io.File(testOutput) / "a.class" / "b" / "c"
+    blocking.mkdirs()
+    blocking.createFile()
+    compileCode("class a")
   }
 }
