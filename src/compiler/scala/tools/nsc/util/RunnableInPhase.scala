0efe66a1a8595f462d9736d2d39c7e23990d7f6c
Mike Skells <mike.skells@talk21.com>
2017-04-13 00:55:18 +0100

    improve documentation a little
    (cherry picked from commit 575e2bb)

--- src/compiler/scala/tools/nsc/util/RunnableInPhase.scala
+++ src/compiler/scala/tools/nsc/util/RunnableInPhase.scala
@@ -10,10 +10,17 @@ object RunnableInPhase {
 }
 /**
   * a wrapper to allow Runnables to be associated to a phase. Primarily useful for profiling
+  *
+  * When you are running a compilation task that involved some activty on a background thread
+  * (not the one running [[Global.compileUnits]]) the profiler is not aware of that thread and so cannot account
+  * for the activity.
+  *
+  * By enclosing the activity in the doRun method of this class the profiler (if enabled) is informed
+  * and the statistics can be gathered
   */
 abstract class RunnableInPhase(global: Global, val phase:Phase, val comment:String) extends Runnable {
 
-  val id = RunnableInPhase.idGen.incrementAndGet()
+  final val id = RunnableInPhase.idGen.incrementAndGet()
   private val profiler = global.currentRun.profiler
   private var idle = 0L
   profiler.registerRunnable(this)
@@ -24,8 +31,19 @@ abstract class RunnableInPhase(global: Global, val phase:Phase, val comment:Stri
     try doRun()
     finally profiler.afterRunnable(this, idle)
   }
+
+  /**
+    * the compilation activity
+    */
   def doRun() : Unit
 
+  /**
+    * if the compilation activity has some idle time waiting on a future, thn this can be recorded by
+    * using this method to do the wait for you. This allow the profiler to distinguish idle time (waiting for some
+    * related activity to complete, from for example waiting or IO
+    * @param future the future that you are waiting on
+    * @param duration the maximum duration to wait
+    */
   def idle(future: Future[_], duration:Duration = Duration.Inf): Unit = {
     if (!future.isCompleted) {
       val start = System.nanoTime()
