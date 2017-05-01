b40c590db39c93c858cdb5b5967671bcccd41781
Mike Skells <mike.skells@talk21.com>
2017-05-02 00:31:18 +0100

    FreshNamesCreator fast path and reduce garbage

--- src/reflect/scala/reflect/internal/util/FreshNameCreator.scala
+++ src/reflect/scala/reflect/internal/util/FreshNameCreator.scala
@@ -20,8 +20,21 @@ class FreshNameCreator(creatorPrefix: String = "") {
    */
   def newName(prefix: String): String = {
     val safePrefix = NameTransformer.encode(prefix)
-    counters.putIfAbsent(safePrefix, new AtomicLong(0))
-    val idx = counters.get(safePrefix).incrementAndGet()
-    s"$creatorPrefix$safePrefix$idx"
+    var sharedCounter = counters.get(safePrefix)
+    if (sharedCounter eq null) {
+      val newCounter = new AtomicLong(0)
+      sharedCounter = counters.putIfAbsent(safePrefix, newCounter)
+      if (sharedCounter eq null)
+        sharedCounter = newCounter
+    }
+    val idx = String.valueOf(sharedCounter.incrementAndGet())
+
+    //return effectively s"$creatorPrefix$safePrefix$idx", but less garbage
+
+    val res = new Array[Char](creatorPrefix.length + safePrefix.length + idx.length)
+    creatorPrefix.getChars(0,creatorPrefix.length, res, 0)
+    prefix.getChars(0,prefix.length, res, creatorPrefix.length)
+    idx.getChars(0,idx.length, res, creatorPrefix.length+creatorPrefix.length)
+    new String(res)
   }
 }
