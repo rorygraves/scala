104fd85c9a97ba98c11be4ab95fc24c36f75e3cf
Mike Skells <mike.skells@talk21.com>
2018-07-27 17:35:32 -0400

    add a test for vector iterator drop

--- test/junit/scala/collection/immutable/VectorTest.scala
+++ test/junit/scala/collection/immutable/VectorTest.scala
@@ -41,5 +41,72 @@ class VectorTest {
   }
 
   @Test def checkSearch: Unit = SeqTests.checkSearch(Vector(0 to 1000: _*), 15,  implicitly[Ordering[Int]])
-  
+
+
+  @Test
+  def vectorIteratorDrop(): Unit = {
+    val underlying = Vector(0 to 10010: _*)
+
+    val totalSize = underlying.size
+
+    for (start <- 1056 to 10000) {
+      val it = underlying.iterator.drop(start)
+      assertEquals(totalSize - start, it.knownSize)
+      assertEquals(totalSize - start, it.size)
+      assertTrue(it.hasNext)
+      assertEquals(start, it.next())
+    }
+  }
+  def intercept[T <: Throwable: Manifest](fn: => Any): T = {
+    try {
+      fn
+      fail(s"expected a ${manifest[T].runtimeClass.getName} to be thrown")
+      ???
+    } catch {
+      case x: T => x
+    }
+  }
+  @Test
+  def vectorIteratorDropToEnd(): Unit = {
+    val underlying = Vector(0)
+
+    for (start <- List(1,2,3,4,99)) {
+      {
+        val it = underlying.iterator.drop(start)
+        assertFalse(it.hasNext)
+        intercept[NoSuchElementException](it.next)
+        it.drop(0)
+        assertFalse(it.hasNext)
+        it.drop(1)
+        assertFalse(it.hasNext)
+        it.drop(99)
+        assertFalse(it.hasNext)
+        intercept[NoSuchElementException](it.next)
+      }
+
+      {
+        val it = underlying.iterator.drop(start)
+        intercept[NoSuchElementException](it.next)
+        it.drop(0)
+        it.drop(1)
+        it.drop(99)
+        intercept[NoSuchElementException](it.next)
+      }
+    }
+  }
+  @Test
+  def vectorIteratorRepeated(): Unit = {
+    val underlying = Vector(1 to 10001: _*)
+
+
+    for (stepSize <- List(0, 1, 2, 3, 4, 8, 10, 24, 32, 63, 64, 100)) {
+      val it = underlying.iterator
+      for (stepCount <- 1 to 10) {
+        it.drop(stepSize)
+        assertTrue(it.hasNext)
+        val expected = (stepSize + 1) * stepCount
+        assertEquals(expected, it.next())
+      }
+    }
+  }
 }
