2d4f10008c5999b8e3eb19568b5b525ba038c683
Mike Skells <mike.skells@talk21.com>
2018-07-27 06:56:38 -0400

    improve benchmark for iterating a vector

--- test/benchmarks/src/main/scala/scala/collection/immutable/VectorIterationBenchmark.scala
+++ test/benchmarks/src/main/scala/scala/collection/immutable/VectorIterationBenchmark.scala
@@ -16,31 +16,36 @@ import java.util.concurrent.TimeUnit
 class VectorIterationBenchmark {
   @Param(Array("0", "1", "2", "3", "4", "6", "8", "10", "12", "14", "16"
     , "20", "24", "28", "32", "40", "48", "56", "64",
-    "128", "256", "512", "1024", "2048", "65536"))
+    "128", "256", "512", "1024", "2048", "4096", "8192", "16384", "32768", "65536"))
   var size: Int = _
 
   var value: Vector[Any] = _
+  var larger: Vector[Any] = _
 
   @Setup(Level.Trial) def init(): Unit = {
     value = Vector.tabulate(size)(_.toString)
+    larger = value :+ "last"
   }
-  @Benchmark def iterate(bh: Blackhole) = {
-    var i = 0
-    val it = value.iterator
-    while (i < size) {
-      bh.consume(it.next)
-      i += 1
-    }
+//  @Benchmark def iterate(bh: Blackhole) = {
+//    var i = 0
+//    val it = value.iterator
+//    while (i < size) {
+//      bh.consume(it.next)
+//      i += 1
+//    }
+//  }
+//@Benchmark def apply(bh: Blackhole) = {
+//  var i = 0
+//  while (i < size) {
+//    bh.consume(value(i))
+//    i += 1
+//  }
+//}
+  @Benchmark def applySingle(bh: Blackhole) = {
+    bh.consume(larger(size))
   }
-  @Benchmark def apply(bh: Blackhole) = {
-    var i = 0
-    while (i < size) {
-      bh.consume(value(i))
-      i += 1
-    }
-  }
-  @Benchmark def drop = {
-    val it = value.iterator
-    it.drop(size)
-  }
+//  @Benchmark def drop = {
+//    val it = value.iterator
+//    it.drop(size)
+//  }
 }
